b8436867652e71abaeafc7e654b12f90cf8e4f75
Junior <cjuniordev@gmail.com>
2022-11-18 20:03:14 -0300

    first commit

---
+++ lib/exemplo.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DynamicFormFild extends StatelessWidget {
+  final int cdCampoFormulario;
+        "cdCampoSuperior": 0,
+        "dsCampo": "Campo Fora 1",
+        "dsValor": "",
+        "cdLinha": 0,
+        "dsFormula": null,
+        "dsMascara": null,
+        "dsValorInicial": "",
+        "dsTextoAjuda": null,
+        "idTipoCampo": 1,
+        "idTipoValor": 3,
+        "idTipoControle": 1,
+        "vlLimite": 0,
+        "vlCasasDecimais": 2,
+        "vlLimiteSelecao": 0,
+        "stAtributoAdicional": 0,
+        "lstItem": null,
+        "idErro": 0,
+        "idExpressaoRegular": 0,
+        "dsExpressaoRegular": null,
+        "lstPeriodos": null,
+        "lstCamposFilhos": null,
+        "lstColunas": null,
+        "fieldState": {
+          "visible": true,
+          "disabled": false,
+          "required": true,
+          "raiseVisible": false,
+          "raiseDisabled": false,
+          "valueHasChanged": false
+        },
+        "lstEventos": [
+          
+        ],
+        "vlAltura": 0,
+        "vlOrdenacao": 0,
+        "lstLinhas": null,
+        "lstFiltros": null,
+        "cdConsulta": 0,
+        "horizontalAlign": 0,
+        "fontWeight": false,
+        "fontStyle": false,
+        "fontUnderline": false,
+        "fontSize": 0
+
+  const DinamicyForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    dinam
+  }
+}
