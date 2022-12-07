ba1d006c3d6c834d1f0595dec2364912a2022ce8
Junior <cjuniordev@gmail.com>
2022-12-07 19:17:24 -0300

    card

--- lib/src/modules/dashboard/view/home_page.dart
+++ lib/src/modules/dashboard/view/home_page.dart
@@ -14,10 +14,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text(widget.title)),
       bottomNavigationBar: const CustomBottomBar(page: 'home'),
-      body: Container(),
+      body: Column(
+        children: [_card(title: 'Entradas', value: '1000', width: screenSize.width *0.4, height: screenSize.width *0.3, context: context)],
+      ),
+    );
+  }
+
+  Widget _card({
+    required String title,
+    required String value,
+    required double width,
+    required double height,
+    Function()? onTap,
+    required BuildContext context
+     
+  }) {
+    return GestureDetector(
+      onTap: onTap ?? (){} ,
+      child: Container(
+        height: height ,
+        width: width,
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          children: [
+            Text(title),
+            Text(value),
+          ],
+        ),
+      ),
     );
   }
 }
